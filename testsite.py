27307b5bf0b39c39422522765687dc4a954282ac
Ken Goddard <goddardken@outlook.com>
2022-07-31 23:45:16 +0000

    updated python tests

--- testsite.py
+++ testsite.py
@@ -3,9 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException        
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome('C:/codebase/webdriver/chromedriver.exe')
 
+
+driver = webdriver.Chrome('/usr/bin/chromedriver')
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--window-size=1420,1080')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+driver = webdriver.Chrome(chrome_options=chrome_options)
 
 import os
 import json
